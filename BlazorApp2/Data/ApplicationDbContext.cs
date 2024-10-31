a2adae8e8be16886acbe42fc9bf850ecee8dc015
bi81cw <bi81cw@FOT-CELLB22>
2024-10-31 11:33:20 +0000

    Add project files.

---
+++ BlazorApp2/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using BlazorApp2.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp2.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Module> Modules { get; set; }
+    }
+}
---
+++ BlazorApp2/Data/Models/Module.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp2.Data.Models
+{
+    public class Module
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Title { get; set; }
+
+    }
+}
+
+
